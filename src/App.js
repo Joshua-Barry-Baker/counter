5be249b05d77fef5c7c3a5d1c0f639343c3cb573
Joshua-Barry-Baker <josh.barry.baker@googlemail.com>
2020-11-19 10:34:03 +0000

    fundermental formatting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './style.css';
 import Nav from './componets/Nav.js';
+import Article from './componets/Article.js';
 
 function App() {
-  return (
+  return (<>
     <Nav />
-  )
+    <Article />
+  </>)
 }
 
 export default App;
